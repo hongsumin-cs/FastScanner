71a822efaf2f1f50d31b165643e2c2740e55c43a
Hong Sumin <wonmiman09@gmail.com>
2026-05-07 01:26:33 +0900

    Add basic path validation

--- FastScanner.cpp
+++ FastScanner.cpp
@@ -1,6 +1,35 @@
 ﻿#include <iostream>
+#include <string>
+#include <filesystem>
 
-int main()
+using namespace std;
+namespace fs = std::filesystem;
+
+int main(int argc, char* argv[])
 {
-    std::cout << "Hello World!\n";
+	// 인자 개수 부족
+	if (argc < 3) {
+		cout << "Usage: " << argv[0] << " <Target Directory> <Word to Search>";
+		return 0;
+	}
+
+	string targetPath = argv[1];
+	string searchWord = argv[2];
+
+	// 목표 디렉토리 미존재
+	if (!fs::exists(targetPath)) {
+		cout << "Path does not exist: " << targetPath << "\n";
+		return 1;
+	}
+
+	// 폴더가 아님
+	if (!fs::is_directory(targetPath)) {
+		cout << "Path is not a directory: " << targetPath << "\n";
+		return 1;
+	}
+
+	cout << "Scanning in: " << targetPath << "\n";
+	cout << "Searching for: " << searchWord << "\n";
+
+	return 0;
 }
