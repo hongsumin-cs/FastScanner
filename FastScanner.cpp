9986e67da247090c62c71b222f2fd6718df2eaf2
Hong Sumin <wonmiman09@gmail.com>
2026-05-10 00:13:02 +0900

    Add single directory scanning function

--- FastScanner.cpp
+++ FastScanner.cpp
@@ -5,9 +5,21 @@
 using namespace std;
 namespace fs = std::filesystem;
 
+
+void scanDirectory(const string& path) {
+	for (const fs::directory_entry entry : fs::directory_iterator(path)) {
+		if (entry.is_directory()) {
+			std::cout << "Directory: " << entry.path().string() << "\n";
+		}
+
+		else if (entry.is_regular_file()) {
+			std::cout << "File: " << entry.path().string() << "\n";
+		}
+	}
+}
+
 int main(int argc, char* argv[])
 {
-	// 인자 개수 부족
 	if (argc < 3) {
 		cout << "Usage: " << argv[0] << " <Target Directory> <Word to Search>";
 		return 0;
@@ -16,20 +28,18 @@ int main(int argc, char* argv[])
 	string targetPath = argv[1];
 	string searchWord = argv[2];
 
-	// 목표 디렉토리 미존재
 	if (!fs::exists(targetPath)) {
 		cout << "Path does not exist: " << targetPath << "\n";
 		return 1;
 	}
 
-	// 폴더가 아님
 	if (!fs::is_directory(targetPath)) {
 		cout << "Path is not a directory: " << targetPath << "\n";
 		return 1;
 	}
 
 	cout << "Scanning in: " << targetPath << "\n";
-	cout << "Searching for: " << searchWord << "\n";
+	cout << "Searching for: " << searchWord << "\n"; 
 
 	return 0;
 }
