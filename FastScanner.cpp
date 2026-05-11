dc0e7fe6e0c33069d68714e150604557be2a5ed6
Hong Sumin <wonmiman09@gmail.com>
2026-05-11 22:38:07 +0900

    Add recursive directory scanning function

--- FastScanner.cpp
+++ FastScanner.cpp
@@ -6,7 +6,7 @@ using namespace std;
 namespace fs = std::filesystem;
 
 
-void scanDirectory(const string& path) {
+void singleFileScan(const string& path) {
 	for (const fs::directory_entry entry : fs::directory_iterator(path)) {
 		if (entry.is_directory()) {
 			std::cout << "Directory: " << entry.path().string() << "\n";
@@ -15,6 +15,24 @@ void scanDirectory(const string& path) {
 		else if (entry.is_regular_file()) {
 			std::cout << "File: " << entry.path().string() << "\n";
 		}
+	}
+}
+
+void recursiveFileScan(const std::string& path) {
+	try {
+		for (const fs::directory_entry entry : fs::directory_iterator(path, fs::directory_options::skip_permission_denied)) {
+			if (entry.is_directory()) {
+				recursiveFileScan(entry.path().string());
+			}
+
+			else if (entry.is_regular_file()) {
+				std::cout << "File: " << entry.path().string() << "\n";
+			}
+		}
+	}
+
+	catch (const fs::filesystem_error& e) {
+		std::cerr << "Error: " << path << "\n";
 	}
 }
 
